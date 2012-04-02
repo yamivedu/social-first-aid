42ce5a8e82b23200512bebea7186aa368ab4165b
sean.sk.chung@gmail.com <sean.sk.chung@gmail.com@30fb6b23-d4ae-92d4-f256-2ea4134bfab4>
2012-04-02 22:33:33 +0000

    Test page has example implementation for "Difficult Child" section.

--- trunk/SocialFirstAid/gen/edu/hawaii/ics466/R.java
+++ trunk/SocialFirstAid/gen/edu/hawaii/ics466/R.java
@@ -40,11 +40,24 @@ public final class R {
         public static final int test=0x7f020008;
         public static final int test_select=0x7f020009;
     }
+    public static final class id {
+        public static final int ChildMisbehave1=0x7f070001;
+        public static final int Correct=0x7f070002;
+        public static final int Question=0x7f070000;
+        public static final int Review=0x7f070005;
+        public static final int Submit=0x7f070004;
+        public static final int Wrong=0x7f070003;
+        public static final int next=0x7f070006;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int childmistest=0x7f030000;
+        public static final int childmistest2=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
+        public static final int childmis1=0x7f050002;
+        public static final int childmis2=0x7f050003;
         public static final int hello=0x7f050000;
     }
     public static final class style {
