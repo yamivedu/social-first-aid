b428d82792a5a184a0c3e9029ca9b79a4c8edc1b
ardell.s.klemme@gmail.com <ardell.s.klemme@gmail.com@30fb6b23-d4ae-92d4-f256-2ea4134bfab4>
2012-04-04 18:28:40 +0000

    adding a few things for info area.  Not committing all my changes or new files yet.

--- trunk/SocialFirstAid/src/edu/hawaii/ics466/InfoActivity.java
+++ trunk/SocialFirstAid/src/edu/hawaii/ics466/InfoActivity.java
@@ -3,9 +3,16 @@ package edu.hawaii.ics466;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Sets the current Display in the Info tab
@@ -13,7 +20,7 @@ import android.widget.TextView;
  * @author Sean Chung
  *
  */
-public class InfoActivity extends Activity {
+public class InfoActivity extends ListActivity {
 	
   private DatabaseHelper helper;
   private SQLiteDatabase database;
@@ -21,19 +28,36 @@ public class InfoActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     
+    setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, LOCATIONS));
+    
+    ListView lv = getListView();
+    lv.setTextFilterEnabled(true);
+    
+    lv.setOnItemClickListener(new OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), ((TextView) view).getText(), 
+            Toast.LENGTH_SHORT).show();
+      }
+    });
 
-    TextView textview = new TextView(this);
-    String s = "";
-    helper = new DatabaseHelper (this);
-    database = helper.getReadableDatabase();
-    
-    List <Content> list = helper.getAllContent(database);
-    for(int i = 0; i < list.size(); i++) {
-    	s += list.get(i) +"\n";
-    }
     
     
-    textview.setText(s);
-    setContentView(textview);
+
+//    TextView textview = new TextView(this);
+//    String s = "";
+//    helper = new DatabaseHelper (this);
+//    database = helper.getReadableDatabase();
+//    
+//    List <Content> list = helper.getAllContent(database);
+//    for(int i = 0; i < list.size(); i++) {
+//    	s += list.get(i) +"\n";
+//    }
+//    
+//    
+//    textview.setText(s);
+//    setContentView(textview);
+    
   }
+  static final String[] LOCATIONS = new String[] {"Home", "Public", "School", "Work"};
 }
