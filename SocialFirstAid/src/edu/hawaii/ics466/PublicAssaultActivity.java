c8a54fe47b98af64c6c428aa3d8a6fb121fb37c7
ardell.s.klemme <ardell.s.klemme@gmail.com>
2012-04-05 20:17:24 +0000

    attempting to resolve conflicts

--- SocialFirstAid/src/edu/hawaii/ics466/PublicAssaultActivity.java
+++ SocialFirstAid/src/edu/hawaii/ics466/PublicAssaultActivity.java
@@ -64,16 +64,21 @@ public class PublicAssaultActivity extends ExpandableListActivity {
   public class MyExpandableListAdapter extends BaseExpandableListAdapter {
       // Sample data set.  children[i] contains the children (String[]) for groups[i].
     private String [] groups = {
-        "1. Call the authorities now.",
-        "2. Don't be a hero.",
-        "3. Be smart.",
-        "4. Only assist with the help of others.",
-        "5. Protect yourself legally.",
-        "6. Administer aid."
+        " 1. Call the authorities now.",
+        " 2. Don't be a hero.",
+        " 3. Be smart.",
+        " 4. Only assist with the help of others.",
+        " 5. Protect yourself legally.",
+        " 6. Administer aid."
     };
     private String[][] children = {
-        {"step1", "step2", "step3"},
-        {"step1", "step2"}
+        {"Always immediately dial 911 if you witness a public assault.  If you're in a club or establishment, you can contact security personnel.  This will ensure that the appropriate people arrive on the scene to handle the situation."},
+        {"Entering into an altercation by yourself, can often make matters worse.  It can and will most likely result in physical harm to yourself and further physical harm to the parties already involved.  In the event that one of the parties involved has a weapon, you may be seriously injured or killed."},
+        {"Don't yell at, or threaten those involved in the assault.  They may decide that you'll be their next target.  Safety is in numbers, so be sure to get to a more populated area and inform the others of the situation."},
+        {"If other people around are willing to intercede to stop the conflict, make sure that there is enough manpower and numbers to safely end the conflict.  Be sure to plan carefully and be organized when you attempt to subdue the assailant."},
+        {"Don't go in with fists swinging, or you may be charged with assault. Make sure you have enough people aiding you such that you can safely and effectively restrain and subdue the assailant until the authorities arrive."},
+        {"If the assailant is properly subdued and is not in danger of administering further acts of violence, then you can proceed to administer aid to the victim.  Be aware that if the victim is not seriously injured, they may seek to retaliate against this assailant.  Attempt to restrain and calm the victim, encouraging them to wait for the authorities to administer justice.  If they are seriously injured, call for an ambulance and have anyone around who is qualified administer first aid."}
+        
     };
 
       public Object getChild(int groupPosition, int childPosition) {
