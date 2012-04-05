3fe77a74cccc877cb7e5df4871c2c3699c3129f2
ardell.s.klemme@gmail.com <ardell.s.klemme@gmail.com@30fb6b23-d4ae-92d4-f256-2ea4134bfab4>
2012-04-05 20:05:53 +0000

    attempting to resolve conflicts

--- trunk/SocialFirstAid/src/edu/hawaii/ics466/InfoActivity.java
+++ trunk/SocialFirstAid/src/edu/hawaii/ics466/InfoActivity.java
@@ -1,62 +1,85 @@
 package edu.hawaii.ics466;
 
-import java.util.List;
-
-import android.app.Activity;
 import android.app.ListActivity;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Sets the current Display in the Info tab
  * 
- * @author Sean Chung
+ * @author Sean Chung, Ardell Klemme
  *
  */
 public class InfoActivity extends ListActivity {
-	
-  private DatabaseHelper helper;
-  private SQLiteDatabase database;
-  
+
+  //  private DatabaseHelper helper;
+  //  private SQLiteDatabase database;
+
+  TextView selection;
+  ListItem[] locations = {
+      new ListItem("Home", HomeActivity.class),
+      new ListItem("Public", PublicActivity.class),
+      new ListItem("School", SchoolActivity.class),
+      new ListItem("Work", WorkActivity.class)
+  };
+
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
-    setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, LOCATIONS));
-    
-    ListView lv = getListView();
-    lv.setTextFilterEnabled(true);
-    
-    lv.setOnItemClickListener(new OnItemClickListener() {
-      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getApplicationContext(), ((TextView) view).getText(), 
-            Toast.LENGTH_SHORT).show();
-      }
-    });
-
-    
-    
-
-//    TextView textview = new TextView(this);
-//    String s = "";
-//    helper = new DatabaseHelper (this);
-//    database = helper.getReadableDatabase();
-//    
-//    List <Content> list = helper.getAllContent(database);
-//    for(int i = 0; i < list.size(); i++) {
-//    	s += list.get(i) +"\n";
-//    }
-//    
-//    
-//    textview.setText(s);
-//    setContentView(textview);
-    
+    setListAdapter(new ArrayAdapter<ListItem>(
+        this,android.R.layout.simple_list_item_1, locations));
+    selection = (TextView) findViewById(R.id.info_location);
   }
-  static final String[] LOCATIONS = new String[] {"Home", "Public", "School", "Work"};
+
+
+  @Override
+  protected void onListItemClick(ListView lview, View view, int position, long id) {
+    super.onListItemClick(lview, view, position, id);
+    final Intent intent = new Intent(this, locations[position].getActivity());
+    startActivityForResult(intent, position);
+  }
+  
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    super.onActivityResult(requestCode, resultCode, intent);
+    if (resultCode == RESULT_OK) {
+      switch (requestCode) {
+      case 0:
+        // TODO: handle return of HomeActivity
+        break;
+      case 1:
+        // TODO: handle return of PublicActivity
+        break;
+      case 2:
+        // TODO: handle return of SchoolActivity
+        break;
+      case 3:
+        // TODO: handle return of WorkActivity
+        break;
+      default:
+        break;
+      }
+    }
+    else if (resultCode == RESULT_CANCELED) {
+      // TODO: handle cancelation
+    }
+      
+  }
+
+  //    TextView textview = new TextView(this);
+  //    String s = "";
+  //    helper = new DatabaseHelper (this);
+  //    database = helper.getReadableDatabase();
+  //    
+  //    List <Content> list = helper.getAllContent(database);
+  //    for(int i = 0; i < list.size(); i++) {
+  //    	s += list.get(i) +"\n";
+  //    }
+  //    
+  //    
+  //    textview.setText(s);
+  //    setContentView(textview);
 }
